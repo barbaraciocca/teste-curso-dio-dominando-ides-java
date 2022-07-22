16841bc13ba9ad69a84ff1c36d47fa57df7b89c0
barbaraciocca <barbara_ciocca@hotmail.com>
2022-07-21 21:48:03 -0300

    first commit

---
+++ src/com/dio/primeiroprograma.java
@@ -0,0 +1,19 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		
+		System.out.println(gato);
+		// TODO Auto-generated method stub
+		/*int a = 1;
+		int b = 2;
+		System.out.println("Hello world!" * (a + b));*/
+	}
+
+}
